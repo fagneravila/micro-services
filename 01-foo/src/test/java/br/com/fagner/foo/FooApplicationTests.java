0a452a593db9768fe29b59b4d94da5fe65692d75
Fagner Avila <fagner_avila99@hotmail.com>
2022-04-19 01:06:33 -0300

    inicial commit

---
+++ 01-foo/src/test/java/br/com/fagner/foo/FooApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fagner.foo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
